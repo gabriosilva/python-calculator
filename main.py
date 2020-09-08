8746c1ecb26204e290b028833a2ca72ef16f632d
gabrio Lina da Silva <MexSolutions@outlook.com>
2020-09-08 20:33:24 -0300

    imported "operator", added banner string, added user input, declared opVal class, declared final(opVal) class, added breakInput function - split the input by inserted pattern or symbol, added process function - execute the math operation,

---
+++ main.py
@@ -0,0 +1,32 @@
+import operator
+
+banner      = print("[+] Welcome to this simple calculator\n")
+operationInput   = input("[I] Type any value to be calculated and its function separated by a comma. ex.: (1,+,1): ")
+
+
+class opVal:
+    def __init__(self, raw, splitedBy):
+        self.ops = ops = {"+":operator.add, "-":operator.sub, "*":operator.mul, "**":operator.pow}
+        self.raw = raw
+        self.splitedBy = splitedBy
+
+
+class final(opVal):
+    def __init__(self, raw, splitedBy):
+        super().__init__(raw, splitedBy)
+
+        def breakInput(raw,splitedBy):
+            return raw.split(str(splitedBy))
+    
+        def process(splitedValue,ops):
+            v0 = int(splitedValue[0])
+            v1 = int(splitedValue[2])
+            op = splitedValue[1]
+            return  ops[op](v0,v1)
+
+        self.splitedValue = breakInput(self.raw,self.splitedBy)
+        self.result = process(self.splitedValue,self.ops)
+
+oper = final(operationInput,',')
+result = oper.result
+print(result)
